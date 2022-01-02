786f6056de086374f281513fc858d092bb33f112
Amirali Monjar <amirali.mnj@gmail.com>
2022-01-02 11:58:33 +0330

    Added logos to main page animation

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import AboutMe from "../components/AboutMe";
 import Header from "../components/Header";
 import "./MainPage.css";
-
+import { DiJava } from "react-icons/di";
+import {
+  SiCplusplus,
+  SiDocker,
+  SiJavascript,
+  SiPython,
+  SiUnity,
+  SiUnrealengine,
+  SiVisualstudiocode,
+} from "react-icons/si";
+import { FaCodeBranch, FaGamepad } from "react-icons/fa";
 const MainPage = () => {
   const [nav, setNav] = React.useState("about-me");
   const handleNaveChange = (selectedNav: string) => {
@@ -12,16 +22,39 @@ const MainPage = () => {
     <>
       <div className="area">
         <ul className="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <FaCodeBranch color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <SiVisualstudiocode
+              color="rgba(255, 255, 255, 0.158)"
+              size={"100%"}
+            />
+          </li>
+          <li>
+            <SiCplusplus color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <SiJavascript color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <DiJava color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <SiUnity color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <SiDocker color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <FaGamepad color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <SiUnrealengine color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
+          <li>
+            <SiPython color="rgba(255, 255, 255, 0.158)" size={"100%"} />
+          </li>
         </ul>
       </div>
       <div>
